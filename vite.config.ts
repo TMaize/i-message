cd6b225962f5354e1bde9cfb4bb3b1aeac263f83
tym <1772314831@qq.com>
2022-10-26 10:48:01 +0800

    chore: workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,35 +3,39 @@ import { resolve } from 'path'
 import { defineConfig, UserConfigExport } from 'vite'
 import dts from 'vite-plugin-dts'
 
-const isProduction = process.env.NODE_ENV === 'production'
 
-const config: UserConfigExport = {
-  build: {
-    sourcemap: false
-  },
-  plugins: [
-    dts({
-      afterBuild() {
-        fs.rmSync('./dist/vite-env.d.ts')
-        fs.renameSync('./dist/lib', './dist/dts')
-      }
-    })
-  ]
-}
-
-if (isProduction) {
-  config.build!.lib = {
-    entry: resolve(__dirname, 'src/lib/main.ts'),
-    name: 'IMessage',
-    fileName: format => `i-message.${format}.js`
-  }
-} else {
-  config.build!.rollupOptions = {
-    input: {
-      index: resolve(__dirname, 'index.html'),
-      child: resolve(__dirname, 'child.html'),
+export default defineConfig(({ mode }) => {
+  const config: UserConfigExport = {
+    base: './',
+    build: {
+      sourcemap: false
     }
   }
-}
 
-export default defineConfig(config)
+  if (mode !== 'lib') {
+    config.build!.rollupOptions = {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        child: resolve(__dirname, 'child.html'),
+      }
+    }
+  }
+
+  if (mode === 'lib') {
+    config.plugins = [
+      dts({
+        afterBuild() {
+          fs.rmSync('./dist/vite-env.d.ts')
+          fs.renameSync('./dist/lib', './dist/dts')
+        }
+      })
+    ]
+    config.build!.lib = {
+      entry: resolve(__dirname, 'src/lib/main.ts'),
+      name: 'IMessage',
+      fileName: format => `i-message.${format}.js`
+    }
+  }
+
+  return config
+})
